63b4601ecd39ab6c4bc1a71afad77b4762357169
Cristina Sanz <cristina.sanz508@comunidadunir.net>
2026-02-16 18:13:36 +0100

    Comentamos las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { NewComponent } from './pages/new/new';
 
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent },
-    {path:'home', component:HomeComponent },
-    {path:'new', component:NewComponent }
+    {path:'', component:HomeComponent }, //dirige directamente al home
+    {path:'home', component:HomeComponent }, //si estamos en al ruta home redrige al home
+    {path:'new', component:NewComponent } //si hacemos clic en añadir serie redrige a new
 ];
